9164bf22352c3f4a30d39af42122f84e23548c2b
pipipopy0 <hanovidf22@gmail.com>
2026-03-03 21:41:23 +0300

    Системные изменения

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-import parser as p
-import time 
-import json
-
+import logics as l
 
 
 if __name__ == "__main__":
-    id = input()
-    with open("links.json", "r", encoding = "utf-8") as f:
-        data = json.load(f)
-        start = time.perf_counter()#засекает время
-        for entry in data:#перебирает все словари
-            for name, link in entry.items():
-                person_dict = p.mpei_parser(id=id, link=link)
-                p_position = person_dict.get("p_position")
-                p_sum_results = person_dict.get("p_sum_results")
-                p_id = person_dict.get("p_id")
-                if str(p_id) == id:
-                    print(f"Абитуриент {p_id} на месте {p_position} с баллами {p_sum_results} на направлении {name}",f" {link} ")
-        end = time.perf_counter()
-        print(f"Выполнение кода потребовало: {end-start:.2f} сек")
-            
-            
+    id_input = input("Введи id: ")
+    l.check_id_directions(id=id_input)
+    
